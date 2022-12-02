a0f312d68b35a4ddd2959616e5803fef73a88737
technology-me <lishangmingxuan@foxmail.com>
2022-12-02 17:03:54 +0800

    [style] 主题完善

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -7,6 +7,7 @@ import workspace from "../workspace/workspace";
 class BoxyNavigation {
   constructor() {
     this.boxyMenuJQ = $(".boxyMenu");
+    this.extendedName = ".boxy";
   }
 
   load = () => {
@@ -38,16 +39,15 @@ class BoxyNavigation {
     const input = document.createElement("input");
     input.setAttribute("type", "file");
     input.setAttribute("name", "file");
-    input.setAttribute("style", "visibility:hidden");
-    input.setAttribute("accept", ".json");
+    input.setAttribute("accept", this.extendedName);
     input.addEventListener("change", function () {
       const file = this.files[0];
       const reader = new FileReader();
       reader.addEventListener("load", function () {
         const json = JSON.parse(this.result || "");
-        Blockly.serialization.workspaces.load(json, workspace.workspace);
+        Blockly.serialization.workspaces.load(json["content"], workspace.workspace);
         console.log("[Boxy] Opened:");
-        console.log(this.result);
+        console.log(JSON.stringify(json, null, "  "));
       });
       reader.readAsText(file);
     });
@@ -57,8 +57,8 @@ class BoxyNavigation {
   save_ = () => {
     this.boxyMenuJQ.hide();
     const json = Blockly.serialization.workspaces.save(workspace.workspace);
-    const data = { name: "Boxy Project Demo", boxy_version: 1, content: json };
-    const text = JSON.stringify(data);
+    const data = { name: "Boxy Project Demo", editorVersion: 1, content: json };
+    const text = JSON.stringify(data, null, "  ");
     const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
     const url = URL.createObjectURL(blob);
     const anchor = document.createElement("a");
@@ -76,7 +76,7 @@ class BoxyNavigation {
     const url = URL.createObjectURL(blob);
     const anchor = document.createElement("a");
     anchor.href = url;
-    anchor.download = "boxy.js";
+    anchor.download = "boxy" + ".js";
     anchor.click();
     console.log("[Boxy] Generated:");
     console.log(jsCode);
