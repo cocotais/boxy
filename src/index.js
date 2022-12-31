24b059e58cc1723d56bc0674a0790bdda2804c05
小鱼yuzifu <yuzifu@yuzifu.top>
2022-12-31 11:01:00 +0800

    [fix] 取消注释sw

--- src/index.js
+++ src/index.js
@@ -59,15 +59,15 @@ trashcanObserver.observe(document.querySelector("#workspace > div > div.blocklyT
   attributeFilter: ["class"],
 });
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/service-worker.js")
-//       .then((registration) => {
-//         console.log("SW registered: ", registration);
-//       })
-//       .catch((registrationError) => {
-//         console.log("SW registration failed: ", registrationError);
-//       });
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
