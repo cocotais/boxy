391f1be8e37a80f70601d03b2f5c6c67c07e997f
technology-me <lishangmingxuan@foxmail.com>
2023-01-02 21:27:56 +0800

    [style] 背景色active

--- src/theme/codemao.renderer.js
+++ src/theme/codemao.renderer.js
@@ -1,46 +1,13 @@
-/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @fileoverview The full custom renderer built during the custom renderer
- * codelab, in ES6 syntax.
- * @author fenichel@google.com (Rachel Fenichel)
- */
 import Blockly from "blockly";
+
 class CustomConstantsProvider extends Blockly.zelos.ConstantProvider {
   constructor() {
-    // Set up all of the constants from the base provider.
     super();
 
-    // Override a few properties.
-    /**
-     * The width of the notch used for previous and next connections.
-     * @type {number}
-     * @override
-     */
     this.NOTCH_WIDTH = 0;
-
-    /**
-     * The height of the notch used for previous and next connections.
-     * @type {number}
-     * @override
-     */
     this.NOTCH_HEIGHT = 0;
 
-    /**
-     * Rounded corner radius.
-     * @type {number}
-     * @override
-     */
     this.CORNER_RADIUS = 4;
-    /**
-     * The height of the puzzle tab used for input and output connections.
-     * @type {number}
-     * @override
-     */
     this.TAB_WIDTH = 10;
     this.TAB_HEIGHT = 10;
 
@@ -49,6 +16,7 @@ class CustomConstantsProvider extends Blockly.zelos.ConstantProvider {
 
     this.EMPTY_INLINE_INPUT_PADDING = 20;
     this.EXTERNAL_VALUE_INPUT_PADDING = 10;
+
     this.FIELD_TEXT_FONTSIZE = 12;
     this.FIELD_TEXT_FONTWEIGHT = "500";
     this.FIELD_TEXT_FONTFAMILY = "sans-serif";
