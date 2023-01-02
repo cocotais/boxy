a11b1e67de25c8b7631d2d5090ee846b04747141
小鱼yuzifu <yuzifu@yuzifu.top>
2023-01-02 19:34:00 +0800

    [style] 没完全搞好的render

--- src/theme/codemao.renderer.js
+++ src/theme/codemao.renderer.js
@@ -10,7 +10,7 @@
  * @author fenichel@google.com (Rachel Fenichel)
  */
 import Blockly from "blockly";
-class CustomConstantsProvider extends Blockly.blockRendering.ConstantProvider {
+class CustomConstantsProvider extends Blockly.zelos.ConstantProvider {
   constructor() {
     // Set up all of the constants from the base provider.
     super();
@@ -41,113 +41,24 @@ class CustomConstantsProvider extends Blockly.blockRendering.ConstantProvider {
      * @type {number}
      * @override
      */
-    this.TAB_WIDTH = 0;
-    this.TAB_HEIGHT = 0;
+    this.TAB_WIDTH = 10;
+    this.TAB_HEIGHT = 10;
 
-    this.FIELD_BORDER_RECT_RADIUS = 4;
-  }
-  /**
-   * @override
-   */
-  init() {
-    super.init();
-    // Add calls to create shape objects for the new connection shapes.
-    this.RECT_PREV_NEXT = this.makeRectangularPreviousConn();
-    this.RECT_INPUT_OUTPUT = this.makeRectangularInputConn();
-  }
+    this.FIELD_BORDER_RECT_RADIUS = 2;
+    this.FIELD_BORDER_RECT_HEIGHT = 10;
 
-  /**
-   * @override
-   */
-  shapeFor(connection) {
-    const checks = connection.getCheck();
-    switch (connection.type) {
-      case Blockly.INPUT_VALUE:
-      case Blockly.OUTPUT_VALUE:
-        // Includes doesn't work in IE.
-        if (checks && checks.indexOf("Number") !== -1) {
-          return this.RECT_INPUT_OUTPUT;
-        }
-        if (checks && checks.indexOf("String") !== -1) {
-          return this.RECT_INPUT_OUTPUT;
-        }
-        return this.PUZZLE_TAB;
-      case Blockly.PREVIOUS_STATEMENT:
-      case Blockly.NEXT_STATEMENT:
-        return this.NOTCH;
-      default:
-        throw Error("Unknown type");
-    }
-  }
-
-  /**
-   * Return a rectangular notch for use with previous and next connections.
-   */
-  makeRectangularPreviousConn() {
-    const width = this.NOTCH_WIDTH;
-    const height = this.NOTCH_HEIGHT;
-
-    /**
-     * Since previous and next connections share the same shape
-     * you can define a function to generate the path for both.
-     */
-    function makeMainPath(dir) {
-      return Blockly.utils.svgPaths.line([
-        Blockly.utils.svgPaths.point(0, height),
-        Blockly.utils.svgPaths.point(dir * width, 0),
-        Blockly.utils.svgPaths.point(0, -height),
-      ]);
-    }
-    const pathLeft = makeMainPath(1);
-    const pathRight = makeMainPath(-1);
-
-    return {
-      width: width,
-      height: height,
-      pathLeft: pathLeft,
-      pathRight: pathRight,
-    };
-  }
-
-  /**
-   * Return a rectangular puzzle tab for use with input and output connections.
-   */
-  makeRectangularInputConn() {
-    const width = this.TAB_WIDTH;
-    const height = this.TAB_HEIGHT;
-
-    /**
-     * Since input and output connections share the same shape you can
-     * define a function to generate the path for both.
-     */
-    function makeMainPath(up) {
-      return Blockly.utils.svgPaths.line([
-        Blockly.utils.svgPaths.point(-width, 0),
-        Blockly.utils.svgPaths.point(0, -1 * up * height),
-        Blockly.utils.svgPaths.point(width, 0),
-      ]);
-    }
-
-    const pathUp = makeMainPath(1);
-    const pathDown = makeMainPath(-1);
-
-    return {
-      width: width,
-      height: height,
-      pathDown: pathDown,
-      pathUp: pathUp,
-    };
+    this.EMPTY_INLINE_INPUT_PADDING = 20;
+    this.EXTERNAL_VALUE_INPUT_PADDING = 10;
+    this.FIELD_TEXT_FONTSIZE = 12;
+    this.FIELD_TEXT_FONTWEIGHT = "500";
+    this.FIELD_TEXT_FONTFAMILY = "sans-serif";
   }
 }
 
-class CustomRenderer extends Blockly.blockRendering.Renderer {
+class CustomRenderer extends Blockly.zelos.Renderer {
   constructor(name) {
     super(name);
   }
-
-  /**
-   * @override
-   */
   makeConstants_() {
     return new CustomConstantsProvider();
   }
