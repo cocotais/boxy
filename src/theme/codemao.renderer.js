a8668479c3112da1dd58986726633c6b551cfb92
小鱼yuzifu <yuzifu@yuzifu.top>
2023-01-03 13:16:50 +0800

    [style]仿codemao积木块

--- src/theme/codemao.renderer.js
+++ src/theme/codemao.renderer.js
@@ -5,21 +5,50 @@ class CustomConstantsProvider extends Blockly.zelos.ConstantProvider {
     super();
 
     this.NOTCH_WIDTH = 0;
-    this.NOTCH_HEIGHT = 0;
+    this.NOTCH_HEIGHT = 8;
 
     this.CORNER_RADIUS = 4;
-    this.TAB_WIDTH = 10;
-    this.TAB_HEIGHT = 10;
-
-    this.FIELD_BORDER_RECT_RADIUS = 2;
-    this.FIELD_BORDER_RECT_HEIGHT = 10;
-
-    this.EMPTY_INLINE_INPUT_PADDING = 20;
-    this.EXTERNAL_VALUE_INPUT_PADDING = 10;
+    this.TAB_WIDTH = 0;
+    this.TAB_HEIGHT = 0;
 
     this.FIELD_TEXT_FONTSIZE = 12;
     this.FIELD_TEXT_FONTWEIGHT = "500";
     this.FIELD_TEXT_FONTFAMILY = "sans-serif";
+  }
+
+  /**
+   * @override
+   */
+  init() {
+    super.init();
+    // Add calls to create shape objects for the new connection shapes.
+    this.RECT_PREV_NEXT = this.makeNotch();
+  }
+
+  makeNotch() {
+    const width = 0;
+    const height = 0;
+
+    /**
+     * Since previous and next connections share the same shape
+     * you can define a function to generate the path for both.
+     */
+    function makeMainPath(dir) {
+      return Blockly.utils.svgPaths.line([
+        Blockly.utils.svgPaths.point(0, height),
+        Blockly.utils.svgPaths.point(dir * width, 0),
+        Blockly.utils.svgPaths.point(0, -height),
+      ]);
+    }
+    const pathLeft = makeMainPath(1);
+    const pathRight = makeMainPath(-1);
+
+    return {
+      width: width,
+      height: height + this.NOTCH_HEIGHT,
+      pathLeft: pathLeft,
+      pathRight: pathRight,
+    };
   }
 }
 
