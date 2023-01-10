b2a937a73add5d1faa9914e28cf53599ce4f29b3
technology-me <lishangmingxuan@foxmail.com>
2023-01-10 11:23:41 +0800

    [fix]  修复代码生成错误

--- src/generators/lua.js
+++ src/generators/lua.js
@@ -2,7 +2,7 @@ import { luaGenerator } from "blockly/lua";
 
 luaGenerator["controls_forever"] = function (block) {
   const branch = luaGenerator.statementToCode(block, "DO");
-  return "while(true) do" + branch + "end";
+  return "while(true) do\n" + branch + "end\n";
 };
 
 luaGenerator["math_evenly_divisible"] = function (block) {
